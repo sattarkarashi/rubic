965e55487c5b953550b75178f8d179cad9a578f0
sattarkarashi <satokarashi@gmail.com>
2024-01-02 12:21:52 +0100

    Draw the primary ans simple skethc for the rubic

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import numpy as np
 
-    """ I am going to vectorize different sides of 
-    the rubic cube dynamically as the first first idea.
-    """
+""" I am going to vectorize different sides of 
+the rubic cube dynamically as the first first idea.
+"""
+    
+## Define the Rubic
+
+class Rubic:
+    def __init__():
+        # get the initial state of the rubic
+    
+    """ We suppose we have a Rubic cube in our hands,
+    it has right, left, up, down moves """
+    def move_right_left():
+        pass
+    
+    def move_up_down():
+        pass
+    
+    
+    
